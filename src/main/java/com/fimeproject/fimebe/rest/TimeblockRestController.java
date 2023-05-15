008d9c6e760fd27e1873bd73e3ad7166c2a9d478
Wilson <chwchiu@connect.ust.hk>
2023-05-16 02:43:47 +0800

    fix: cors error

--- src/main/java/com/fimeproject/fimebe/rest/TimeblockRestController.java
+++ src/main/java/com/fimeproject/fimebe/rest/TimeblockRestController.java
@@ -2,6 +2,7 @@ package com.fimeproject.fimebe.rest;
 
 import com.fimeproject.fimebe.dao.TimeblockDAO;
 import com.fimeproject.fimebe.entity.Timeblock;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class TimeblockRestController {
     }
 
     // expose "/timeblocks" and return a list of all timeblocks
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/timeblocks")
     public List<Timeblock> findAll() {
         return timeblockDAO.findAll();
